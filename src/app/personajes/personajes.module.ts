86ce94abc7d28a971daa4a7e6ce43ee72a4f94ba
Nicolás Jiménez Quintero <51033293+nijimenez@users.noreply.github.com>
2019-10-15 17:49:52 -0500

    Modulo y Componente de Personajes

---
+++ src/app/personajes/personajes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonajesComponent } from './personajes.component';
+
+
+
+@NgModule({
+  declarations: [PersonajesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonajesModule { }
