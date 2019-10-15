599e9d7eafb477f3bec5426e9e40af9c034b46b2
Nicolás Jiménez Quintero <51033293+nijimenez@users.noreply.github.com>
2019-10-15 17:46:19 -0500

    Modulo y Componente de Especies

---
+++ src/app/especies/especies.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EspeciesComponent } from './especies.component';
+
+
+
+@NgModule({
+  declarations: [EspeciesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EspeciesModule { }
