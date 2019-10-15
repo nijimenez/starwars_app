122a5d74d074593d5b9297368ab4d9dd9d4f51e0
Nicolás Jiménez Quintero <51033293+nijimenez@users.noreply.github.com>
2019-10-15 17:50:37 -0500

    Modulo y Componente de Planetas

---
+++ src/app/planetas/planetas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlanetasComponent } from './planetas.component';
+
+
+
+@NgModule({
+  declarations: [PlanetasComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PlanetasModule { }
