87bdb1a262651c3c035e882fcfde37ef14f16b19
Nicolás Jiménez Quintero <51033293+nijimenez@users.noreply.github.com>
2019-10-15 17:48:59 -0500

    Modulo y Componente de Naves

---
+++ src/app/naves/naves.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavesComponent } from './naves.component';
+
+
+
+@NgModule({
+  declarations: [NavesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavesModule { }
